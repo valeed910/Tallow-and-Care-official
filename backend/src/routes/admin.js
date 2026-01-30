53189178fb5872ced538ae1b8729c3710cced1eb
Mohd Valeed <mohdvaleedhasan@gmail.com>
2026-01-30 18:14:12 +0530

    Handle Admin Panel

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -14,7 +14,15 @@ router.use((req, res, next) => {
 
 // get all messages
 router.get("/messages", async (req, res) => {
-  const messages = await Message.find().sort({ createdAt: -1 });
+  const page = Number(req.query.page || 1);
+  const limit = 10;
+  const skip = (page - 1) * limit;
+
+  const messages = await Message.find()
+    .sort({ createdAt: -1 })
+    .skip(skip)
+    .limit(limit);
+
   res.json(messages);
 });
 
