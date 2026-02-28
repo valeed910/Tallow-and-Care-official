4e6d8d858f30854f7ce098eebb4411084c28ed3f
Mohd Valeed <mohdvaleedhasan@gmail.com>
2026-02-28 22:24:09 +0530

    Bcrypt implement

---
+++ backend/src/models/User.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6
+    }
+  },
+  {
+    timestamps: true // automatically adds createdAt & updatedAt
+  }
+);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+const User = mongoose.model("User", userSchema);
+export default User;
