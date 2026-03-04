0b1d889ce3b147e3e0edff6d9c5d4880d372907d
Mohd Valeed <mohdvaleedhasan@gmail.com>
2026-03-04 16:19:46 +0530

    Remove next function in User

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -27,12 +27,11 @@ const userSchema = new mongoose.Schema(
     timestamps: true // automatically adds createdAt & updatedAt
   }
 );
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
-  const salt = await bcrypt.genSalt(10);
+ const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
